2246b635ef58f6900eb99319a896bfe86e28a2bc
Aditya Ozalwar <adityaozalwarcnp@gmail.com>
2024-09-26 16:11:57 +0530

    final

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-   const app = express();
-   const port = 3000;
+const app = express();
+const port = 3000;
 
-   app.get('/', (req, res) => {
-     res.send('Hello, World! Update');
-   });
+app.get('/', (req, res) => {
+  res.send('Hello, World by Devops Engineer!');
+});
 
-   app.listen(port, () => {
-     console.log(`App is running on http://localhost:${port}`);
-   });
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
